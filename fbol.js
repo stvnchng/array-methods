fc17f404fe94dbce7f2c26b6f5dce2676108f7d3
Steven <46513711+stvnchng@users.noreply.github.com>
2022-10-16 22:02:23 -0500

    change fbol to take in a start and end

--- fbol.js
+++ fbol.js
@@ -1,5 +1,5 @@
-const fbol = (upto) => {
-  [...Array(upto + 1).keys()].filter(i => i !== 0).forEach((i) => console.log("Fizz".repeat(Number(i % 3 === 0)) + "Buzz".repeat(Number(i % 5 === 0)) || i));
+const fbol = (start = 1, end) => {
+  [...Array(end + 1).keys()].filter(i => i >= start).forEach((i) => console.log("Fizz".repeat(Number(i % 3 === 0)) + "Buzz".repeat(Number(i % 5 === 0)) || i));
 };
 
-fbol(100)
+fbol(1, 100)
