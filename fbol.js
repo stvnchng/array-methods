8d1f0d15591c3c216307917edbae88674176d54b
Steven <46513711+stvnchng@users.noreply.github.com>
2022-10-17 10:06:43 -0500

    the TRUE fbol

--- fbol.js
+++ fbol.js
@@ -1,5 +1,3 @@
-const fbol = (start = 1, end) => {
-  [...Array(end + 1).keys()].filter(i => i >= start).forEach((i) => console.log("Fizz".repeat(Number(i % 3 === 0)) + "Buzz".repeat(Number(i % 5 === 0)) || i));
-};
+const fbol = (i) => [...Array(i).keys()].forEach((i) => i > 0 && console.log("Fizz".repeat(i % 3 === 0) + "Buzz".repeat(i % 5 === 0) || i));
 
-fbol(1, 100)
+fbol(101);
