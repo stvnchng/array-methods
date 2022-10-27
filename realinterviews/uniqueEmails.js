95983b84fc90929d5f45292ccf8eb1a4af7627cf
Steven <46513711+stvnchng@users.noreply.github.com>
2022-10-26 23:43:18 -0500

    add uniqueEmails solution

---
+++ realinterviews/uniqueEmails.js
@@ -0,0 +1,13 @@
+/**
+ *
+ **/
+const numUniqueEmails = (emails) => {
+  const uniqueEmails = new Set();
+  for (const email of emails) {
+    let [local, domain] = email.split("@");
+    local = local.split("+")[0].split(".").join("");
+    uniqueEmail = local + "@" + domain;
+    uniqueEmails.add(uniqueEmail);
+  }
+  return uniqueEmails.size;
+};
