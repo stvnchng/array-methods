2a0900750d103397c862ec0a394660fcf89db94d
Steven <46513711+stvnchng@users.noreply.github.com>
2022-11-08 16:36:54 -0600

    fix tree, add valid bst and pretty print with tests

--- ds/tree.js
+++ ds/tree.js
@@ -1,14 +1,45 @@
+/**
+ * Implementation of a binary tree with left and right children.
+ */
 class TreeNode {
-  constructor(val, left = undefined, right = undefined) {
+  constructor(val, left = null, right = null) {
     this.val = val;
     this.left = left;
     this.right = right;
   }
+}
 
-  traverse() {
-    console.log(this.val);
-    if (this.left) this.left.traverse();
-    if (this.right) this.right.traverse();
+/**
+ * Inorder traversal of binary tree
+ */
+function traverse(root) {
+  console.log(root.val);
+  if (root.left) traverse(root.left);
+  if (root.right) traverse(root.right);
+}
+
+/**
+ * Checks if the tree is a valid BST.
+ * The conditions for a valid BST are as follows:
+ * - The leftside children of a node must be less in value
+ * - The rightside children of a node must be greater in value
+ */
+function isValid(root, min = -Infinity, max = Infinity) {
+  if (!root) return true;
+  if (root.val <= min || root.val >= max) return false;
+  const left = isValid(root.left, min, root.val);
+  const right = isValid(root.right, root.val, max);
+  return left && right;
+}
+
+/**
+ * Pretty-print tree
+ */
+function printTree(root, n = 0) {
+  if (root) {
+    printTree(root.right, n + 1);
+    console.log(" ".repeat(4 * n) + "-> " + root.val);
+    printTree(root.left, n + 1);
   }
 }
 
@@ -17,4 +48,8 @@ const tree = new TreeNode(
   new TreeNode(2, new TreeNode(3)),
   new TreeNode(4, undefined, new TreeNode(5, new TreeNode(6)))
 );
-tree.traverse();
+printTree(tree);
+console.log("tree.validBST() should expect false:", isValid(tree));
+const tree2 = new TreeNode(2, new TreeNode(1), new TreeNode(3));
+printTree(tree2);
+console.log("tree2.validBST() should expect true:", isValid(tree2));
